b35e235367ddc351641eaa084a50c09f67833230
Marcos Emanuel <marcos.length@gmail.com>
2021-02-23 13:24:10 -0300

    Create Config

---
+++ src/config/multer.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const multer = require('multer')
+
+module.exports = {
+    dest: path.resolve(__dirname, "..", "uploads"),
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, path.resolve(__dirname, "..", "uploads"))
+        },
+        filename: (req, file, cb) => {
+            const nome = file.originalname
+            cb(null, nome)
+        }
+    }),
+    limits: {
+        fileSize: 2 * 1024 * 1024
+    },
+    fileFilter: (req, file, cb) => {
+        const allowedMimes = [
+            'image/jpeg',
+            'image/pjpeg',
+            'image/jpg',
+            'image/png',
+            'image/gif'
+        ]
+
+        if (allowedMimes.includes(file.mimetype)) {
+            cb(null, true)
+        } else {
+            cb(new Error('Formato inválido'))
+        }
+    }
+}
