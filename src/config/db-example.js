48b558889c803fbfbfc6e66bddd7eca35ce32835
Marcos Emanuel <marcos.length@gmail.com>
2021-02-23 13:26:51 -0300

    Add example database config

---
+++ src/config/db-example.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: 'mysql',
+    host: '', /* HOST */
+    username: '', /* USER */ 
+    password: '', /* PASSWORD */
+    database: '', /* NAME */
+    define: {
+        timestamps: false
+    }
+}
