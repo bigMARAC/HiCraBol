4aadaef21f8f852583c7fb4f555ae37a59755568
Marcos Emanuel <marcos.length@gmail.com>
2021-02-25 10:31:01 -0300

    Add auth middleare for upload img route

---
+++ src/middlewares/AuthMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const Aluno = require('./../models/Aluno')
+
+module.exports = {
+    async check(req, res, next) {
+        if (req.headers.authorization) {
+            const [, token] = req.headers.authorization.split(' ')
+
+            const aluno = await Aluno.findOne({
+                where: { token }
+            })
+            if (aluno) {
+                next()
+            } else {
+                res.status(401).json({ erro: 'Usuário não autenticado' })
+            }
+
+        } else {
+            res.status(401).json({ erro: 'Baerer Token não informado' })
+        }
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const AlunoController = require('./controllers/AlunoController')
 const ImageController = require('./controllers/ImageController')
+const Auth = require('./middlewares/AuthMiddleware')
 const multer = require('multer')
 const multerConfig = require('./config/multer')
 
@@ -9,6 +10,6 @@ const routes = express.Router()
 routes.post('/alunos', AlunoController.create)
 routes.post('/alunos/login', AlunoController.auth)
 
-routes.post('/pictures', multer(multerConfig).single("file"), ImageController.create)
+routes.post('/pictures', Auth.check, multer(multerConfig).single("file"), ImageController.create)
 
 module.exports = routes
